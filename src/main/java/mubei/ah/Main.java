e5aa23718bf88c54ea8d3bb6058d142c5c4e5fb7
ah_sxh <2978161783@qq.com>
2023-07-09 14:36:14 +0800

    feat:init

---
+++ src/main/java/mubei/ah/Main.java
@@ -0,0 +1,11 @@
+package mubei.ah;
+
+/**
+ * @author 帅小伙呀
+ * @date 2023/7/9 14:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
